975e0a5d8f13cfa210282672808d3de6bdcddb2a
qttq23 <1753102@student.hcmus.edu.vn>
2021-05-09 22:01:27 +0700

    init

---
+++ background.js
@@ -0,0 +1,116 @@
+
+// create only 1 window for translate?? when user click too fast: click icon + click search context???
+// detect window after close???
+// handle runtime.lastError???
+// make translate window close when click at 'reload' button on extension manager page???
+
+
+console.log('background.js: hello');
+let windowGgTrans = null;
+
+
+// create window when icon clicked
+async function createTranslateWindow() {
+
+  return new Promise((resolve, reject)=>{
+
+    chrome.windows.create({
+      url: 'https://translate.google.com/',
+      type: 'popup',
+      width: 400,
+      height: 500
+
+    }, function(createdWindow){
+
+      console.log(createdWindow);
+      windowGgTrans = createdWindow;
+      resolve(createdWindow);
+    });
+
+  });
+
+
+}
+
+// remove reference when window is removed
+chrome.windows.onRemoved.addListener(id=>{
+  if(windowGgTrans && windowGgTrans.id == id) {
+    windowGgTrans = null;
+  }
+});
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log('click at braction');
+
+  createTranslateWindow();
+
+});
+
+
+// create context menu
+let contextMenuItems = [
+{
+  id: '1',
+  title: 'search with google translate',
+  type: 'normal',
+  contexts: ['selection'],
+
+},
+];
+
+for(let item of contextMenuItems){
+  chrome.contextMenus.create(item);  
+}
+
+
+// handle context menu clicked
+chrome.contextMenus.onClicked.addListener(async function(info, tab){
+
+  // text
+  let selectedText = info.selectionText;
+  console.log(selectedText);
+
+  // window id
+  if(!windowGgTrans || !windowGgTrans.id) {
+    await createTranslateWindow();
+  }
+  let windowId = windowGgTrans.id;
+  
+
+  // tabid
+  chrome.tabs.query({
+    windowId: windowId,
+    index: 0,
+    // url: startwith translate...??
+  }, (tabs)=>{
+
+
+    let sendToTranslateWindow = (selectedText) => {
+
+      // send request to contentscript
+      chrome.tabs.sendMessage(tabs[0].id, {
+        type: 'search',
+        input: selectedText 
+      },
+      null,
+      (response)=>{
+
+        if(!response) {
+          console.log('set retry');
+          setTimeout(()=>{sendToTranslateWindow(selectedText);}, 1000);
+          return;
+        }
+        console.log(response);
+
+      });
+
+      // focus on translate window
+      chrome.windows.update(windowId, {focused: true});
+    };
+    sendToTranslateWindow(selectedText);
+
+  });
+
+
+});
---
+++ contentScript.js
@@ -0,0 +1,55 @@
+
+chrome.runtime.onMessage.addListener((message, sender, sendReponse)=>{
+
+	if(message.type == 'search') {
+
+		try {
+			let pasteToInputPanel = (text)=>{
+				let translateInputTextArea = document.querySelector("textarea.er8xn");
+
+				if(text){
+					translateInputTextArea.value = text;    
+					translateInputTextArea.select();
+					document.execCommand("copy");
+				}
+
+				translateInputTextArea.value="";
+				translateInputTextArea.focus();
+				document.execCommand("paste");
+				translateInputTextArea.blur();
+
+			};
+
+			pasteToInputPanel(message.input);
+
+			sendReponse({
+				type: 'res_search',
+				isOk: true
+			});
+
+		}
+		catch(error){
+			console.log(error);
+
+			sendReponse({
+				type: 'res_search',
+				isOk: false
+			});
+		}
+		
+
+	}
+});
+
+
+
+// optional
+// remove the header bar which is not needed
+(()=>{
+	let headerBar = document.querySelector('#gb > div.gb_Kd.gb_4d.gb_Td.gb_Sd');
+	if(!headerBar) {
+		console.log('not found headerBar');
+		return;
+	}
+	headerBar.style.display = 'none';
+})();
